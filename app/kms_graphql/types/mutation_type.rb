73ee2a3983db369555e64caaa8a643a8c66888f6
Hitesh <erhitesh_2010@yahoo.co.in>
2018-06-27 19:24:53 +0530

    Features# Updates related to Model mutations

--- app/kms_graphql/types/mutation_type.rb
+++ app/kms_graphql/types/mutation_type.rb
@@ -1,24 +1,17 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  field :createCollection, Types::Kms::ModelType do
-    description "Create a Collection."
-    
-    argument :kms_model_name, !types.String
-    argument :collection_name, !types.String
-    argument :description, !types.String
-    argument :allow_creation_using_form, !types.Boolean
-    
-    resolve ->(obj, args, ctx) {
-      Kms::Model.create!(
-        kms_model_name: args[:kms_model_name],
-        collection_name: args[:collection_name].parameterize.underscore,
-        description: args[:description],
-        allow_creation_using_form: args[:allow_creation_using_form]
-      )
-    }
-  end
+  # # Collections
+  # Create Collection
+  field :createCollection, Mutations::Kms::ModelMutations::Create.field
 
+  # Update Collection
+  field :updateCollection, Mutations::Kms::ModelMutations::Update.field
+
+  # Delete Collection
+  field :deleteCollection, Mutations::Kms::ModelMutations::Delete.field
+
+  # # Entries
   # Create Entry for any Collection
   field :createEntry, Mutations::Kms::EntryMutations::Create.field
   
